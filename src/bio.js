a3bae5e4219210ff01782f1366c808acb7a2bd4b
Edgar, Ryan <ryan.edgar@accenture.com>
2020-02-25 14:29:02 -0500

    added pages and styling

---
+++ src/bio.js
@@ -0,0 +1,2 @@
+import "./styles.css";
+// import Image from './assests/re.jpg';
--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
 
-import "./styles.css";
-// import MenuIcon from '@material-ui/icons/Menu';
 
-document.getElementsByClassName("title");
